9a4901059fe0dfc1e6b2c875dee842d4b74873e4
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2020-03-25 15:50:38 +0200

    Person component

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Person from "./Person/Person";
 
 const App = () => {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Person />
+    </div>
+  );
 };
 
 export default App;
---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Person.css";
+
+const Person = () => {
+  return (
+    <div className="persons-card">
+      <p>I am Margit and I am 39 years old.</p>
+    </div>
+  );
+};
+export default Person;
