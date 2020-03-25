f2f264bb8be22daad815898d8e614b3d8c392d0c
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2020-03-25 16:01:32 +0200

    added class component and states

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-      <Person name="Margit" age="39" />
-      <Person name="Criselle" age="20" />
-      <Person name="Karin" age="42" />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Margit", age: 39 },
+      { name: "Criselle", age: 20 },
+      { name: "Karin", age: 42 }
+    ]
+  };
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
+}
 
 export default App;
