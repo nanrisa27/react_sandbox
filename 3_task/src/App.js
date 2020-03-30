c0f5a067223e8ad53ae1c1658a71010eb4bded5d
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2020-03-30 15:03:23 +0300

    added event handlers

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -2,10 +2,32 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    likes: 0
+  };
+
+  addHandler = () => {
+    console.log("addHandler clicked");
+    this.setState({ likes: this.state.likes + 1 });
+  };
+
+  removeHandler = () => {
+    console.log("removeHandler clicked");
+    this.setState({ likes: this.state.likes - 1 });
+  };
+
+  resetHandler = () => {
+    console.log("resetHandler clicked");
+    this.setState({ likes: 0 });
+  };
+
   render() {
     return (
       <div>
-        <h1>Total likes: </h1>
+        <h1>Total likes: {this.state.likes}</h1>
+        <button onClick={this.addHandler}>Add like</button>
+        <button onClick={this.removeHandler}>Remove like</button>
+        <button onClick={this.resetHandler}>Reset likes</button>
       </div>
     );
   }
